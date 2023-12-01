ba96851691c8ba3a8bbfa41a35fb249c5ec6283e
thmsn <9084377+thmsndk@users.noreply.github.com>
2023-12-01 01:07:46 +0000

    updated bee dungeon json for importing map data to db

--- node/server.js
+++ node/server.js
@@ -12037,7 +12037,7 @@ function update_instance(instance) {
 							stype: "spawn",
 							x: spot.x,
 							y: spot.y,
-							target: player.name,
+							target: player.name, // TODO: Targeting players might terrify them.
 							master: monster.id,
 						});
 					}
