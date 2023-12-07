fea641e9d90ad557fb55c5b186a9d3e96977dfbc
thmsn <9084377+thmsndk@users.noreply.github.com>
2023-12-07 19:59:09 +0000

    adopt_extras now handles .gives

--- node/server.js
+++ node/server.js
@@ -6832,7 +6832,7 @@ function init_io() {
 				}
 
 				// Handle map specific & class specific extras
-				adopt_extras(def, def[player.player.type]);
+				adopt_extras(def, def[player.type]);
 				adopt_extras(def, def[player.map]);
 
 				var timeout = 2000;
