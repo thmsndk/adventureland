157c1c1e4b857ad5fcd6b8e4ec43c7204559f184
thmsn <9084377+thmsndk@users.noreply.github.com>
2024-02-25 15:07:10 +0100

    This allows you to do a docker-compose up where your host(s) files are mounted inside  the docker container

--- appserver-entrypoint.sh
+++ appserver-entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+# pwd this scripts runs inside /adventureland/node
+
+# Generate config files if they are missing or the one in usefull is newer.
+echo "Setting up server secrets & environment"
+cp -u ../useful/template.environment.py ../environment.py && \
+cp -u ../useful/template.secrets.py ../secrets.py && \
+cp -u ../useful/template.variables.js ./variables.js && \
+cp -u ../useful/template.live_variables.js ./live_variables.js
+
 exec python2 /appserver/sdk/dev_appserver.py --storage_path=/appserver/storage/ --blobstore_path=/appserver/storage/blobstore/ --datastore_path=/appserver/storage/db.rdbms --admin_host=0.0.0.0 --host=0.0.0.0 --port=8083 /adventureland/ --require_indexes --skip_sdk_update_check
