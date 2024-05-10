ca58a6b4aa57aaf1f6e7edcbe361c947c5c24fca
thmsn <9084377+thmsndk@users.noreply.github.com>
2024-05-10 20:51:24 +0200

    fixed an issue trying to load a map .json file that did not exist

--- api.py
+++ api.py
@@ -1603,14 +1603,16 @@ def create_server_api(**args):
 		else:
 			# Load design/maps/%key json if it exists
 			logging.warning("%s in maps.%s was not found. Trying to import from design/maps/%s.json"%(key,name, key))
-			with open("design/maps/%s.json"%key) as json_file:
-				logging.info("%s.json was found in design/maps saving to db"%key)
-				# Create map
-				map=Map(key=ndb.Key(Map,key),info=GG())
-				map.info.data=json.load(json_file)
-				process_map(map)
-				map.updated=datetime.now()
-				map.put()
+			filename = "design/maps/%s.json"%key 
+			if os.path.exists(filename):
+				with open("design/maps/%s.json"%key) as json_file:
+					logging.info("%s.json was found in design/maps saving to db"%key)
+					# Create map
+					map=Map(key=ndb.Key(Map,key),info=GG())
+					map.info.data=json.load(json_file)
+					process_map(map)
+					map.updated=datetime.now()
+					map.put()
 
 	jhtml(self,{
 		"id":server.k(),
