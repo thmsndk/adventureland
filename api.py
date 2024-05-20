53b7ac1f28423a49a0e84d133bd796727c41b043
thmsn <9084377+thmsndk@users.noreply.github.com>
2024-05-20 21:44:36 +0200

    Load design/maps/%key json if it exists

--- api.py
+++ api.py
@@ -1553,7 +1553,6 @@ def create_server_api(**args):
 	for name in maps:
 		key=maps[name]["key"]
 		if maps[name].get("ignore"): continue
-
 		dbmap=get_by_iid("map|%s"%key)
 		if dbmap:
 			geometry[name]=dbmap.info.data
@@ -1570,7 +1569,7 @@ def create_server_api(**args):
 					process_map(map)
 					map.updated=datetime.now()
 					map.put()
-
+					
 	jhtml(self,{
 		"id":server.k(),
 		"auth":server.info.auth,
