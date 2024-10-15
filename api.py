7491c0f6e57370488e0b0278aa3c7c5e3b1a7f86
thmsn <9084377+thmsndk@users.noreply.github.com>
2024-10-15 17:35:45 +0200

    fixed indentation error in api.py might be from wasteland map branch

--- api.py
+++ api.py
@@ -1552,8 +1552,10 @@ def create_server_api(**args):
 	geometry={}
 	for name in maps:
 		key=maps[name]["key"]
-		if maps[name].get("ignore"): continue
-			dbmap=get_by_iid("map|%s"%key)
+		
+  		if maps[name].get("ignore"): continue
+  
+		dbmap=get_by_iid("map|%s"%key)
 		if dbmap:
 			geometry[name]=dbmap.info.data
 		else:
